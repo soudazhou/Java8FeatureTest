88a1a3cf5791ded570e080701624fc17adc27f00
Wenxuan <soudazhou@gmail.com>
2017-01-16 20:33:52 +0000

    added Stream

--- src/MyDataImpl.java
+++ src/MyDataImpl.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -54,5 +55,25 @@ public class MyDataImpl extends MyDataClass implements MyData {
         Stream<Integer> numbers2 = Stream.of(1,2,3,4,5);
         numbers2.forEach(i -> System.out.print(i+","));
 
+        //match()
+        Stream<Integer> numbers3 = Stream.of(1,2,3,4,5);
+        System.out.println("Stream contains 4? "+numbers3.anyMatch(i -> i==4));
+//Stream contains 4? true
+
+        Stream<Integer> numbers4 = Stream.of(1,2,3,4,5);
+        System.out.println("Stream contains all elements less than 10? "+numbers4.allMatch(i -> i<10));
+//Stream contains all elements less than 10? true
+
+        Stream<Integer> numbers5 = Stream.of(1,2,3,4,5);
+        System.out.println("Stream doesn't contain 10? "+numbers5.noneMatch(i -> i==10));
+//Stream doesn't contain 10? true
+
+
+        //findFirst()
+        Stream<String> names4 = Stream.of("Pankaj","Amit","David", "Lisa");
+        Optional<String> firstNameWithD = names4.filter(i -> i.startsWith("D")).findFirst();
+        if(firstNameWithD.isPresent()){
+            System.out.println("First Name starting with D="+firstNameWithD.get()); //David
+        }
     }
 }
